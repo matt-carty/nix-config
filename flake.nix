95438c645648846a638f4a1b4195f5a5d6e68d2f
Matt <matt@cartycodes.com>
2024-08-17 01:29:11 +0900

    added alien to flake.nix

--- flake.nix
+++ flake.nix
@@ -39,6 +39,12 @@
         # > Our main home-manager configuration file <
         modules = [./home-manager/home.nix];
       };
+      "matt@alien" = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
+        extraSpecialArgs = {inherit inputs outputs;};
+        # > Our main home-manager configuration file <
+        modules = [./home-manager/home.nix];
+      };
     };
   };
 }
