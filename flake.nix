112330fea9e907356456203fb4949538d82e152d
Matt <matt@cartycodes.com>
2024-08-16 22:53:45 +0900

    changed flake.nix to try to fix unknown variable error

--- flake.nix
+++ flake.nix
@@ -8,8 +8,10 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
 
     # Home manager
-    home-manager.url = "github:nix-community/home-manager/release-24.05";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };  
   };
 
   outputs = {
