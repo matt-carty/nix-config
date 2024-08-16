cb3fa3993996c5a0ab3ea4f985367b2f701cc0ba
Matt <matt@cartycodes.com>
2024-08-17 01:32:05 +0900

    renamed home-manager directory

--- flake.nix
+++ flake.nix
@@ -37,13 +37,13 @@
         pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
         extraSpecialArgs = {inherit inputs outputs;};
         # > Our main home-manager configuration file <
-        modules = [./home-manager/home.nix];
+        modules = [./home/home.nix];
       };
       "matt@alien" = home-manager.lib.homeManagerConfiguration {
         pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
         extraSpecialArgs = {inherit inputs outputs;};
         # > Our main home-manager configuration file <
-        modules = [./home-manager/home.nix];
+        modules = [./home/home.nix];
       };
     };
   };
