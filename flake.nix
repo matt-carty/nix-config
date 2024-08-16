80be2c9d598011a7ae5ec1112ead3ff78dc01179
Matt <matt@cartycodes.com>
2024-08-17 02:17:04 +0900

    moved homevm folder

--- flake.nix
+++ flake.nix
@@ -25,7 +25,7 @@
       homevm = nixpkgs.lib.nixosSystem {
         specialArgs = {inherit inputs outputs;};
         # > Our main nixos configuration file <
-        modules = [./nixos/homevm/configuration.nix];
+        modules = [./systems/homevm/configuration.nix];
       };
     };
 
