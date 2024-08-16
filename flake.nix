04267b345c93a792c7e14ac916b2a9ba3287563f
Matt <matt@cartycodes.com>
2024-08-17 01:46:32 +0900

    moved files

--- flake.nix
+++ flake.nix
@@ -43,7 +43,7 @@
         pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
         extraSpecialArgs = {inherit inputs outputs;};
         # > Our main home-manager configuration file <
-        modules = [./home/home.nix];
+        modules = [./home/matt/home.nix];
       };
     };
   };
