0e80feb6ecf9e99ad97e3def0895756ee45b4127
Matt <matt@cartycodes.com>
2024-09-12 12:37:18 +0900

    added LazyVim

--- systems/common/global/default.nix
+++ systems/common/global/default.nix
@@ -8,6 +8,11 @@
 
   environment.systemPackages = with pkgs; [
     home-manager
+    libgccjit # for neovim lazyvim
+    gccStdenv # troubleshooting lazyvim treesitter
+    fd        # for lazyvim 
+    ripgrep   # for lazyvim
+    zoxide
     gh
   ];
 
--- systems/common/optional/desktop/printers.nix
+++ systems/common/optional/desktop/printers.nix
@@ -1,4 +1,11 @@
-{ pkgs, ...}: {
+{ pkgs, services, ...}: {
+# Enable CUPS and wifi compatible printers
+  services.printing.enable = true;
 
+  services.avahi = {
+  enable = true;
+  nssmdns = true;
+  openFirewall = true;
+  };
 
 }
