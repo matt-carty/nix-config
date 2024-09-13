031c44148f1d11380ef8a350a680e4ec0eb45fa1
Matt <matt@cartycodes.com>
2024-09-13 16:36:10 +0900

    started removing lazyvim

--- systems/common/global/default.nix
+++ systems/common/global/default.nix
@@ -8,10 +8,6 @@
 
   environment.systemPackages = with pkgs; [
     home-manager
-    libgccjit # for neovim lazyvim
-    gccStdenv # troubleshooting lazyvim treesitter
-    fd        # for lazyvim 
-    ripgrep   # for lazyvim
     zoxide
     gh
   ];
