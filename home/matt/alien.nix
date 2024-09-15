8f5882c3b088d1eadb8f66c2c578d6d1203380bb
Matt Cartwright <matt@cartycodes.com>
2024-09-15 21:15:46 +0900

    added aliases for home-manager and nixos-rebuild

--- home/matt/alien.nix
+++ home/matt/alien.nix
@@ -6,5 +6,12 @@
     ./common/global/default.nix
     ./common/features/editing.nix
     ./common/features/obsidian.nix
-];
+  ];
+  programs.bash = {
+    enable = true;
+    shellAliases = [
+    hss = "home-manager switch --flake .#matt@alien"
+    nrs = "sudo nixos-rebuild switch --flake .#alien"
+    ];
+
 }
