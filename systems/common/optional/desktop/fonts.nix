157be5f80921a5988a521009433c4102e9a9cade
Matt Cartwright <matt@cartycodes.com>
2024-09-16 23:03:07 +0900

    added Japanese fonts

--- systems/common/optional/desktop/fonts.nix
+++ systems/common/optional/desktop/fonts.nix
@@ -5,4 +5,50 @@
 
   ];
 
+  # Enable fonts to use on your system.  You should make sure to add at least
+  # one English font (like dejavu_fonts), as well as Japanese fonts like
+  # "ipafont" and "kochi-substitute".
+  fonts.packages = with pkgs; [
+    carlito
+    dejavu_fonts
+    ipafont
+    kochi-substitute
+    source-code-pro
+    ttf_bitstream_vera
+  ];
+
+  # These settings enable default fonts for your system.  This setting is very
+  # important.  It lets fontconfig know that you want to fall back to a Japanese
+  # font (for example "IPAGothic") if an application tries to show fonts with
+  # Japanese.  For instance, this is important if you are using a terminal
+  # emulator and you `cat` some Japanese text to the screen. If you don't have
+  # "defaultFonts" configured, fontconfig will pick a random Japanese font to
+  # use.  If you have this "defaultFonts" setting configured, fontconfig will
+  # pick the font you have selected.  This makes sure Japanese fonts look nice.
+  fonts.fontconfig.defaultFonts = {
+    monospace = [
+      "DejaVu Sans Mono"
+      "IPAGothic"
+    ];
+    sansSerif = [
+      "DejaVu Sans"
+      "IPAPGothic"
+    ];
+    serif = [
+      "DejaVu Serif"
+      "IPAPMincho"
+    ];
+  };
+
+
+  # This enables "fcitx" as your IME.  This is an easy-to-use IME.  It supports many different input methods.
+  i18n.inputMethod = {
+   type = "fcitx5";
+   enable = true;
+   fcitx5.addons = with pkgs; [
+     fcitx5-mozc
+     fcitx5-gtk
+   ];
+  };
+
 }
--- systems/common/optional/desktop/gnome.nix
+++ systems/common/optional/desktop/gnome.nix
@@ -1,7 +1,7 @@
 { pkgs, ...}: {
   
   environment.systemPackages = with pkgs; [
-   gnome.gnome-tweaks 
+   gnome-tweaks 
 
   ];
 
