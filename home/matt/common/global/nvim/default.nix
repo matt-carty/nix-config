a8b06b2779761dd45733b19b902a933d2af333b8
Matt <matt@cartycodes.com>
2024-09-16 17:12:31 +0900

    added lsps to neovim

--- home/matt/common/global/nvim/default.nix
+++ home/matt/common/global/nvim/default.nix
@@ -6,14 +6,15 @@
   ...
   }: {
 
-
+  # required packages for plugins etc..
   home.packages = with pkgs; [ 
     lua-language-server
     stylua
     luajitPackages.luarocks
     lua
+    ripgrep
   ]; 
-  
+
   programs.nixvim = {
     
     enable = true;
@@ -32,6 +33,43 @@
     plugins.treesitter.enable = true;
     plugins.web-devicons.enable = true;
     plugins.which-key.enable = true;
+    plugins.neo-tree.enable = true;
+
+    # Language server
+    plugins.lsp = {
+      enable = true;
+      servers = {
+        # Average webdev LSPs
+        ts_ls.enable = true; # TS/JS
+        cssls.enable = true; # CSS
+        tailwindcss.enable = true; # TailwindCSS
+        html.enable = true; # HTML
+        astro.enable = true; # AstroJS
+        phpactor.enable = true; # PHP
+        svelte.enable = false; # Svelte
+        vuels.enable = false; # Vue
+        pyright.enable = true; # Python
+        marksman.enable = true; # Markdown
+        nil-ls.enable = true; # Nix
+        dockerls.enable = true; # Docker
+        bashls.enable = true; # Bash
+        clangd.enable = true; # C/C++
+        csharp-ls.enable = true; # C#
+        yamlls.enable = true; # YAML
+
+        lua-ls = { # Lua
+          enable = true;
+          settings.telemetry.enable = false;
+        };
+
+        # Rust
+        rust-analyzer = {
+          enable = true;
+          installRustc = true;
+          installCargo = true;
+        };
+      };
+    };
 
     # Non-nixvim plugins here
 
@@ -49,5 +87,23 @@
     
     # Keymaps here
     globals.mapleader = " "; # Sets the leader key to space
+  
+    keymaps = [
+      # Neo-tree bindings
+      {
+        action = "<cmd>Neotree toggle<CR>";
+        key = "<leader>e";
+      }
+      # Telescope bindings
+      {
+        action = "<cmd>Telescope find_files<CR>";
+        key = "<leader>ff";
+      }
+      {
+        action = "<cmd>Telescope live_grep<CR>";
+        key = "<leader>fw";
+      }
+    ];
   };
+
 }
