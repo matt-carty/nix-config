c9930d3aa3ff16ae114aaa3f119faf69355b66e0
Matt <matt@cartycodes.com>
2024-09-20 17:32:29 +0900

    added pub key for razorback

--- systems/common/global/users.nix
+++ systems/common/global/users.nix
@@ -15,7 +15,9 @@
       initialPassword = "correcthorsebatterystaple";
       isNormalUser = true;
       openssh.authorizedKeys.keys = [
-        # TODO: Add your SSH public key(s) here, if you plan on using SSH to connect
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBteXQnAI6CZavJTsr/MgDMlIH5gJtAtJXySvaDAc00n matt@razorback"
+
+	# TODO: Add your SSH public key(s) here, if you plan on using SSH to connect
       ];
       # Need to work out if absent groups cause an error? Be sure to add any other groups you need (such as networkmanager, audio, docker, etc)
       extraGroups = ["wheel"];
