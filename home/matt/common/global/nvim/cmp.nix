691fb773098324fe555d4232ad2f95c05b38b882
Matt <matt@cartycodes.com>
2024-09-28 16:49:27 +0900

    completion mappings

--- home/matt/common/global/nvim/cmp.nix
+++ home/matt/common/global/nvim/cmp.nix
@@ -6,7 +6,16 @@
     plugins.cmp = {
       enable = true;
       autoEnableSources = true;
-        settings.sources = [
+      settings = {
+      mapping = cmp.mapping.preset.insert({
+      ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+      ['<C-f>'] = cmp.mapping.scroll_docs(4),
+      ['<C-Space>'] = cmp.mapping.complete(),
+      ['<C-e>'] = cmp.mapping.abort(),
+      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    }),
+    };
+      settings.sources = [
 	  { name = "nvim_lsp"; }
 	  { name = "path"; }
 	  { name = "buffer"; }
