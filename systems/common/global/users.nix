64db8e03ab7ecb9c3d3a7bf90268733648b50efc
Matt <matt@cartycodes.com>
2024-10-12 17:36:11 +0900

    added ssh keys

--- systems/common/global/users.nix
+++ systems/common/global/users.nix
@@ -1,6 +1,4 @@
-{ pkgs,
-  config, 
-  lib,
+{ 
   ...
   }: { 
 
@@ -16,8 +14,10 @@
       isNormalUser = true;
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBteXQnAI6CZavJTsr/MgDMlIH5gJtAtJXySvaDAc00n matt@razorback"
+	"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFf41ijLtS97CqgCgjyMgsNhZTZG/bgsmD29Sk9NbxRP matt@docky"
+	"ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCrDvClKLRmbUPgG1TnBs11udMQr8HmAUTsix44nj2ZwSOZG9NPPFxqRU4AN4mgITecN948B6Nmc2ygpofuXd+ZaLdMeBjduMgA+ZzdUeALhKmrBR4dtozjgdojOlA6rus3/aeCTBfGWUtPP5FmU2nCgmqMBl4XKUYy9yqTd7iAqcY/8QI6wvxBy29v7KWfc7nSncItzG5BI3bWm/s1ByknVMOTpUpc6uMsEHcZwuPHbtEepIrOkWDtS43U0RQk/EuQumx25GoVNHXScnNN5qsYWNqrbOjDGS4QPbC2MFTiZCdNlPPVJF2ZLxERi0EmyWG1H3CM1f6Lq53jQs1hnw0Y0Mn3WEUzwYI00gc1T2r3tMex/zqTT5MIvZLi0a9g4643OOfKdgBPLfhvKnc9KA51KKwq16uPcJv7+9s9vJODZ/9NuefmqzD8oSDYqB7cXWRqVe1FIGhIB75neOq2RQJc/9FIxTgVGn0Isb/mEsSKJoLFZxqteJb1SHmNsNQXsYk= matt@drummer"
 
-	# TODO: Add your SSH public key(s) here, if you plan on using SSH to connect
+	# TODO: Add ssh public key from alien 
       ];
       # Need to work out if absent groups cause an error? Be sure to add any other groups you need (such as networkmanager, audio, docker, etc)
       extraGroups = ["wheel"];
