b44ad67e8b77e1f4ec9d4c20f2f2be7571739357
Matt <matt@cartycodes.com>
2024-11-04 12:56:21 +0900

    fixed autologin warning message

--- systems/common/optional/desktop/autologin.nix
+++ systems/common/optional/desktop/autologin.nix
@@ -4,8 +4,8 @@
   
 {
   # Enable automatic login for the user.
-  services.xserver.displayManager.autoLogin.enable = true;
-  services.xserver.displayManager.autoLogin.user = "matt";
+  services.displayManager.autoLogin.enable = true;
+  services.displayManager.autoLogin.user = "matt";
 
   # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
   systemd.services."getty@tty1".enable = false;
