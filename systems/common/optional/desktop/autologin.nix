ed4394040ca99cc8564d588d555de8d1b5f8feac
Matt <matt@cartycodes.com>
2024-11-04 12:53:31 +0900

    fixing errors

--- systems/common/optional/desktop/autologin.nix
+++ systems/common/optional/desktop/autologin.nix
@@ -4,10 +4,8 @@
   
 {
   # Enable automatic login for the user.
-  services.displayManager = {
-    autoLogin = true;
-    user = "matt";
-    };
+  services.xserver.displayManager.autoLogin.enable = true;
+  services.xserver.displayManager.autoLogin.user = "matt";
 
   # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
   systemd.services."getty@tty1".enable = false;
