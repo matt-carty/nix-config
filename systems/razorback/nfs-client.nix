63e1143c872a7f1ebd4ca50aaea9e16ef7a8d9c8
Matt <matt@cartycodes.com>
2025-04-18 20:04:27 +0900

    added stuff to rz

--- systems/razorback/nfs-client.nix
+++ systems/razorback/nfs-client.nix
@@ -4,4 +4,8 @@ fileSystems."/mnt/nextcloud" = {
     device = "bobbie:/export/nextcloud";
     fsType = "nfs";
 };
+fileSystems."/mnt/stuff" = {
+    device = "bobbie:/export/stuff";
+    fsType = "nfs";
+};
 }
