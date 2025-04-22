1299d04f3d3affb962bf6a0c098eb3f00aa6d80e
Matt <matt@cartycodes.com>
2025-04-22 19:23:04 +0900

    added bind mount for data on razor

--- systems/razorback/nfs-client.nix
+++ systems/razorback/nfs-client.nix
@@ -8,4 +8,9 @@ fileSystems."/mnt/stuff" = {
     device = "bobbie:/stuff";
     fsType = "nfs";
 };
+fileSystems."/home/matt/docker/nextcloud/data/data" = {
+    depends = [ "/mnt/nextcloud" ]; 
+    device = "/mnt/nextcloud/data/data";
+    options = [ "bind" ];
+ };
 }
