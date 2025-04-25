36b45723344ef42dbd67302ca93fa40e059397b9
Matt <matt@cartycodes.com>
2025-04-25 18:29:54 +0900

    removed the bind mount

--- systems/razorback/nfs-client.nix
+++ systems/razorback/nfs-client.nix
@@ -8,9 +8,4 @@ fileSystems."/mnt/stuff" = {
     device = "bobbie:/stuff";
     fsType = "nfs";
 };
-fileSystems."/home/matt/docker/nextcloud/data/data" = {
-    depends = [ "/mnt/nextcloud" ]; 
-    device = "/mnt/nextcloud/data/data";
-    options = [ "bind" ];
- };
 }
