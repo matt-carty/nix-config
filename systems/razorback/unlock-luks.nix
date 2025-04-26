dca8717dcc01878393f15d62d6e8385266c4657c
Matt <matt@cartycodes.com>
2025-04-26 16:00:20 +0900

    troubleshooting ':

--- systems/razorback/unlock-luks.nix
+++ systems/razorback/unlock-luks.nix
@@ -8,8 +8,8 @@ boot.initrd = {
     ssh = {
       enable = true;
       port = 22;
-      authorizedKeys = [ "ssh-rsa AAAAyourpublic-key-here..." ];
-      hostKeys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAxvKmVDcszbYT7/5PfXk/VFJaI59Gzl/mNpg2jxgkOl" ];
+      authorizedKeys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAxvKmVDcszbYT7/5PfXk/VFJaI59Gzl/mNpg2jxgkOl matt@ipad" ];
+      hostKeys = [ "/etc/secrets/initrd/ssh_host_rsa_key" ];
       shell = "/bin/cryptsetup-askpass";
     };
   };
