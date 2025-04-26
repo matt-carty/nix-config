ed765daa388a3072d37e703f9b44dfa2e0634d19
Matt <matt@cartycodes.com>
2025-04-26 16:29:38 +0900

    added mount for ssd

--- systems/razorback/mount-ssd.nix
+++ systems/razorback/mount-ssd.nix
@@ -3,4 +3,15 @@
 environment.etc.crypttab.text =
 ''luks-48a66bfc-3bb1-4a2b-8244-c2a04fc4fcac UUID=48a66bfc-3bb1-4a2b-8244-c2a04fc4fcac /etc/luks-keys/luks-48a66bfc-3bb1-4a2b-8244-c2a04fc4fcac nofail
 '';
+fileSystems = {
+  "/mnt/ssd" = {
+    device = "/dev/mapper/luks-48a66bfc-3bb1-4a2b-8244-c2a04fc4fcac";
+    fsType = "ext4";
+    options = [ "nofail"
+		"rw"
+		"nosuid"
+		"uhelper=udisks2"];
+  };
+}
+;
 }
