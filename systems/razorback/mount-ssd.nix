a55aeb92cf44c17c1cf83b8b2318535ff2e109dc
Matt <matt@cartycodes.com>
2025-04-26 16:22:54 +0900

    trying to get ssd to mount

---
+++ systems/razorback/mount-ssd.nix
@@ -0,0 +1,6 @@
+{ ... }: {
+
+environment.etc.crypttab.text =
+''luks-48a66bfc-3bb1-4a2b-8244-c2a04fc4fcac UUID=48a66bfc-3bb1-4a2b-8244-c2a04fc4fcac /etc/luks-keys/luks-48a66bfc-3bb1-4a2b-8244-c2a04fc4fcac nofail
+'';
+}
