8600eb45238e117a62dafa55c5ba98c126fdf7ee
Matt Cartwright <matt@cartycodes.com>
2025-05-26 14:03:06 +0900

    changed all the pulse audio to services

--- systems/medina/configuration.nix
+++ systems/medina/configuration.nix
@@ -82,7 +82,7 @@ boot.loader.efi.canTouchEfiVariables = true;
   services.printing.enable = true;
 
   # Enable sound with pipewire.
-  hardware.pulseaudio.enable = false;
+  services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
