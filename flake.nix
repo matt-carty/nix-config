38eb264405081beedddc3f317af7816a88987655
Matt Cartwright <matt@cartycodes.com>
2025-06-03 15:02:55 +0900

    troubleshooting

--- flake.nix
+++ flake.nix
@@ -2,7 +2,6 @@
   description = "Your new nix config";
 
   inputs = {
-
     # Nix ecosystem
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.05";
@@ -11,8 +10,8 @@
     # Home manager
     home-manager.url = "github:nix-community/home-manager/";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    
-    # Nix hardware 
+
+    # Nix hardware
     hardware.url = "github:nixos/nixos-hardware";
 
     # Sops - not yet implemented
@@ -27,7 +26,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-                # Optional, if you intend to follow nvf's obsidian-nvim input
+    # Optional, if you intend to follow nvf's obsidian-nvim input
     # you must also add it as a flake input.
     obsidian-nvim.url = "github:epwalsh/obsidian.nvim";
 
@@ -38,73 +37,71 @@
       # instance of nixpkgs. This is safe to do as nvf does not depend
       # on a binary cache.
       inputs.nixpkgs.follows = "nixpkgs";
+    };
 
-  };
-
-  outputs =
-    {
+    outputs = {
       self,
       nixpkgs,
       home-manager,
       nixvim,
-                        nvf,
+      nvf,
       ...
-    }@inputs:
-    let
+    } @ inputs: let
       inherit (self) outputs;
-    in
-    {
+    in {
       # NixOS configuration entrypoint
       # Available through 'nixos-rebuild --flake .#your-hostname'
       nixosConfigurations = {
-	# Laptop for home
+        # Laptop for home
         alien = nixpkgs.lib.nixosSystem {
           specialArgs = {
             inherit inputs outputs;
           };
           # > Our main nixos configuration file <
-          modules = [ ./systems/alien/configuration.nix ];
+          modules = [./systems/alien/configuration.nix];
         };
-	# NUC server for skippy
+        # NUC server for skippy
         razorback = nixpkgs.lib.nixosSystem {
           specialArgs = {
             inherit inputs outputs;
           };
           # > Our main nixos configuration file <
-          modules = [ ./systems/razorback/configuration.nix ];
+          modules = [./systems/razorback/configuration.nix];
         };
-	# RPi 4 file server at skippy
+        # RPi 4 file server at skippy
         behemoth = nixpkgs.lib.nixosSystem {
           specialArgs = {
             inherit inputs outputs;
           };
           # > Our main nixos configuration file <
-          modules = [ ./systems/behemoth/configuration.nix ];
-	};
-	# RPi 4 server at skippy - maybe wont use
+          modules = [./systems/behemoth/configuration.nix];
+        };
+        # RPi 4 server at skippy - maybe wont use
         bobbie = nixpkgs.lib.nixosSystem {
           specialArgs = {
             inherit inputs outputs;
           };
           # > Our main nixos configuration file <
-          modules = [ ./systems/bobbie/configuration.nix ];
-	};
-	# RPi 3 server at skippy 
+          modules = [./systems/bobbie/configuration.nix];
+        };
+        # RPi 3 server at skippy
         holden = nixpkgs.lib.nixosSystem {
           specialArgs = {
             inherit inputs outputs;
           };
           # > Our main nixos configuration file <
-          modules = [ ./systems/holden/configuration.nix ];
-	};
-	# HP Z400 workstation at skippy  (main pc)
+          modules = [./systems/holden/configuration.nix];
+        };
+        # HP Z400 workstation at skippy  (main pc)
         medina = nixpkgs.lib.nixosSystem {
           specialArgs = {
             inherit inputs outputs;
           };
           # > Our main nixos configuration file <
-          modules = [ nvf.nixosModules.default
-                ./systems/medina/configuration.nix ];
+          modules = [
+            nvf.nixosModules.default
+            ./systems/medina/configuration.nix
+          ];
         };
       };
 
@@ -117,7 +114,7 @@
             inherit inputs outputs;
           };
           # > Our main home-manager configuration file <
-          modules = [ ./home/matt/alien.nix ];
+          modules = [./home/matt/alien.nix];
         };
         "matt@razorback" = home-manager.lib.homeManagerConfiguration {
           pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
@@ -125,7 +122,7 @@
             inherit inputs outputs;
           };
           # > Our main home-manager configuration file <
-          modules = [ ./home/matt/razorback.nix ];
+          modules = [./home/matt/razorback.nix];
         };
         "matt@medina" = home-manager.lib.homeManagerConfiguration {
           pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
@@ -133,8 +130,10 @@
             inherit inputs outputs;
           };
           # > Our main home-manager configuration file <
-          modules = [ nvf.homeManagerModules.default
-                ./home/matt/medina.nix ];
+          modules = [
+            nvf.homeManagerModules.default
+            ./home/matt/medina.nix
+          ];
         };
         "matt@behemoth" = home-manager.lib.homeManagerConfiguration {
           pkgs = nixpkgs.legacyPackages.aarch64-linux; # Home-manager requires 'pkgs' instance
@@ -142,7 +141,7 @@
             inherit inputs outputs;
           };
           # > Our main home-manager configuration file <
-          modules = [ ./home/matt/behemoth.nix ];
+          modules = [./home/matt/behemoth.nix];
         };
         "matt@bobbie" = home-manager.lib.homeManagerConfiguration {
           pkgs = nixpkgs.legacyPackages.aarch64-linux; # Home-manager requires 'pkgs' instance
@@ -150,7 +149,7 @@
             inherit inputs outputs;
           };
           # > Our main home-manager configuration file <
-          modules = [ ./home/matt/bobbie.nix ];
+          modules = [./home/matt/bobbie.nix];
         };
         "matt@holden" = home-manager.lib.homeManagerConfiguration {
           pkgs = nixpkgs.legacyPackages.aarch64-linux; # Home-manager requires 'pkgs' instance
@@ -158,8 +157,9 @@
             inherit inputs outputs;
           };
           # > Our main home-manager configuration file <
-          modules = [ ./home/matt/holden.nix ];
+          modules = [./home/matt/holden.nix];
         };
       };
     };
+  };
 }
