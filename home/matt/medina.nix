ab857848a663648a06c9392f081610eb55e285d1
Matt Cartwright <matt@cartycodes.com>
2025-06-03 16:59:20 +0900

    :eyeroll:

--- home/matt/medina.nix
+++ home/matt/medina.nix
@@ -1,101 +1,102 @@
-{
-  ...
-}: {
+{...}: {
   imports = [
     ./common/global/default.nix
     ./common/features/editing.nix
     ./common/features/obsidian.nix
     ./common/features/gen-desktop.nix
-  ]; 
+  ];
 
   # Customisations for matt@medina
   programs.kitty = {
     settings = {
       font_size = 11;
     };
-   };
+  };
   programs.nvf = {
     enable = true;
-        settings = {
-       
-        vim = {
-
-        lsp = { 
+    settings = {
+      vim = {
+        lsp = {
           enable = true;
-                                        
+          formatOnSave = true;
           trouble.enable = true;
         };
-       languages =  { 
-       
-         enableFormat = true;
-         enableTreesitter = true;
+        languages = {
+          enableFormat = true;
+          enableTreesitter = true;
 
-         nix.enable = true; 
-         markdown.enable = true;
-        };  
+          nix.enable = true;
+          markdown.enable = true;
+        };
 
         theme = {
-      enable = true;
-      name = "rose-pine";
-      style = "main";
-      };
-                                autocomplete = {
-      blink-cmp.enable = true;
-    };
-                                statusline = {
-      lualine = {
-        enable = true;
-        theme = "auto";
-      };
-    };
-                                filetree = {
-      neo-tree = {
-        enable = true;
-      };
-    }; 
-                                 visuals = {
-      nvim-scrollbar.enable = true;
-      nvim-web-devicons.enable = true;
-      nvim-cursorline.enable = true;
-      cinnamon-nvim.enable = true;
-      fidget-nvim.enable = true;
-
-      highlight-undo.enable = true;
-      indent-blankline.enable = true;
-
-      # Fun
-      cellular-automaton.enable = false;
-    }; 
-                                treesitter.context.enable = true;
-
-    binds = {
-      whichKey.enable = true;
-      cheatsheet.enable = true;
-      hardtime-nvim.enable = true;
-    };
-
-    telescope.enable = true;
-
-    git = {
-      enable = true;
-      gitsigns.enable = true;
-      gitsigns.codeActions.enable = false; # throws an annoying debug message
-    };
-
-    minimap = {
-      minimap-vim.enable = false;
-      codewindow.enable = true; # lighter, faster, and uses lua for configuration
-    };
-
-    dashboard = {
-      dashboard-nvim.enable = false;
-      alpha.enable = true;
-    };
-
-    notify = {
-      nvim-notify.enable = true;
-    };
+          enable = true;
+          name = "rose-pine";
+          style = "main";
         };
+        autocomplete = {
+          blink-cmp.enable = true;
+        };
+        statusline = {
+          lualine = {
+            enable = true;
+            theme = "auto";
+          };
+        };
+        filetree = {
+          neo-tree = {
+            enable = true;
+          };
+        };
+        visuals = {
+          nvim-scrollbar.enable = true;
+          nvim-web-devicons.enable = true;
+          nvim-cursorline.enable = true;
+          cinnamon-nvim.enable = true;
+          fidget-nvim.enable = true;
+
+          highlight-undo.enable = true;
+          indent-blankline.enable = true;
+
+          # Fun
+          cellular-automaton.enable = false;
+        };
+        treesitter.context.enable = true;
+
+        binds = {
+          whichKey.enable = true;
+          cheatsheet.enable = true;
+          hardtime-nvim.enable = true;
+        };
+
+        telescope.enable = true;
+
+        git = {
+          enable = true;
+          gitsigns.enable = true;
+          gitsigns.codeActions.enable = false; # throws an annoying debug message
+        };
+
+        minimap = {
+          minimap-vim.enable = false;
+          codewindow.enable = true; # lighter, faster, and uses lua for configuration
+        };
+
+        dashboard = {
+          dashboard-nvim.enable = false;
+          alpha.enable = true;
+        };
+
+        notify = {
+          nvim-notify.enable = true;
+        };
+        terminal = {
+          toggleterm = {
+            enable = true;
+            lazygit.enable = true;
+          };
+        };
+      };
     };
-};
+  };
 }
