c029e9e4f99b5bfeefd3d8e0cb6382798d7c34c9
Matt Cartwright <matt@cartycodes.com>
2025-06-06 17:59:17 +0900

    raspberry pi desktop

---
+++ systems/common/optional/desktop/gnone-pi.nix
@@ -0,0 +1,25 @@
+{ pkgs, ...}: {
+  
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.lightdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.videoDriver = [ "fbdev" ];
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+  
+  # Packages primarily related to using gnome
+  environment.systemPackages = with pkgs; [
+   gnome-tweaks
+   gnomeExtensions.smart-auto-move
+   libheif
+
+  ];
+
+}
