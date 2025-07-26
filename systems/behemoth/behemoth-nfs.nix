1db4505fb2e9e372943c03c26481e1eddcf60c1a
Matt Cartwright <matt@cartycodes.com>
2025-07-26 17:24:02 +0900

    still working on nfs

--- systems/behemoth/behemoth-nfs.nix
+++ systems/behemoth/behemoth-nfs.nix
@@ -34,8 +34,8 @@
   services.nfs.server.enable = true;
   services.nfs.server.exports = ''
     /export         10.89.24.0/24(rw,fsid=0,no_subtree_check) 10.89.42.0/24(rw,fsid=0,no_subtree_check)
-    /export/flint	10.89.24.0/24(rw,fsid=0,no_subtree_check) 10.89.42.0/24(rw,fsid=0,no_subtree_check)
-    /export/stuff 10.89.24.0/24(rw,fsid=0,no_subtree_check) 10.89.42.0/24(rw,fsid=0,no_subtree_check)
+    /export/flint	10.89.24.0/24(rw,fsid=1,no_subtree_check) 10.89.42.0/24(rw,fsid=1,no_subtree_check)
+    /export/stuff 10.89.24.0/24(rw,fsid=2,no_subtree_check) 10.89.42.0/24(rw,fsid=2,no_subtree_check)
   '';
   networking.firewall.enable = false;
 }
