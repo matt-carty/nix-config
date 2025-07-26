098bcc2fcc7190251e6ab99adaf9f3040b3e3e39
Matt Cartwright <matt@cartycodes.com>
2025-07-26 17:03:21 +0900

    file rename

--- systems/behemoth/configuration.nix
+++ systems/behemoth/configuration.nix
@@ -14,7 +14,7 @@
 
     # You can also split up your configuration and import pieces of it here:
     ../common/global/default.nix
-    ./bobbie-nfs.nix
+    ./behemoth-nfs.nix
     # Import your generated (nixos-generate-config) hardware configuration
     ./hardware-configuration.nix
   ];
