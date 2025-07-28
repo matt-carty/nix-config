4873dfa34992391f9edf44ff06db427c4b31b842
Matt Cartwright <matt@cartycodes.com>
2025-07-28 12:17:57 +0900

    working on nfs settings to get jelly running;a

--- systems/behemoth/behemoth-nfs.nix
+++ systems/behemoth/behemoth-nfs.nix
@@ -33,8 +33,8 @@
   };
   services.nfs.server.enable = true;
   services.nfs.server.exports = ''
-    /export         10.89.24.0/24(rw,fsid=0,no_subtree_check) 10.89.42.0/24(rw,fsid=0,no_subtree_check)
-    /export/flint	10.89.24.0/24(rw,fsid=1,no_subtree_check) 10.89.42.0/24(rw,fsid=1,no_subtree_check)
+    /export         10.89.24.0/24(rw,fsid=0,no_subtree_check,no_root_squash) 10.89.42.0/24(rw,fsid=0,no_subtree_check,no_root_squash)
+    /export/flint	10.89.24.0/24(rw,fsid=1,no_subtree_check,no_root_squash) 10.89.42.0/24(rw,fsid=1,no_subtree_check,no_root_squash)
     /export/stuff 10.89.24.0/24(rw,fsid=2,no_subtree_check) 10.89.42.0/24(rw,fsid=2,no_subtree_check)
   '';
   networking.firewall.enable = false;
