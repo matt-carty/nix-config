c908144be7e679777d91ce69c8571814b30bf890
Matt Cartwright <matt@cartycodes.com>
2025-08-08 10:44:05 +0900

    added appimage and cursor

--- home/matt/common/features/gen-desktop.nix
+++ home/matt/common/features/gen-desktop.nix
@@ -1,16 +1,18 @@
-
-{ pkgs, ...}: {
-
+{pkgs, ...}: {
   home.packages = with pkgs; [
-    
-   calibre 
-   vlc
-   remmina
-   pdfslicer
-   vial
+    calibre
+    vlc
+    remmina
+    pdfslicer
+    vial
+    appimage-run
   ];
-programs.gnome-shell = {
-  enable = true;
-#  extensions = [{ package = pkgs.gnomeExtensions.gsconnect; }];
-};
+  programs.gnome-shell = {
+    enable = true;
+    #  extensions = [{ package = pkgs.gnomeExtensions.gsconnect; }];
+  };
+
+  programs.bash.shellAliases = {
+    cursor = "appimage-run /home/matt/appimage/Cursor-1.4.2-x86_64.AppImage";
+  };
 }
