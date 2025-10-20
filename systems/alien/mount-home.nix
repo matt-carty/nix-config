25d0260d4663528e3239c483e633c8293231e29b
Matt Cartwright <matt@cartycodes.com>
2025-10-20 14:24:22 +0900

    had to reinstall alien and add home drive mount

---
+++ systems/alien/mount-home.nix
@@ -0,0 +1,8 @@
+{ ... }:{
+
+fileSystems."/home" =
+    { device = "/dev/disk/by-uuid/86745910-5dfc-4038-93ae-00c3ec6ef566";
+      fsType = "ext4";
+    };
+
+  boot.initrd.luks.devices."luks-e15f276f-164f-4619-b3df-dd5593629fb4".device = "/dev/disk/by-uuid/e15f276f-164f-4619-b3df-dd5593629fb4";
