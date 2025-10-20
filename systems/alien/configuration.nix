9766b81713012acef4524f223c13be863ab98210
Matt Cartwright <matt@cartycodes.com>
2025-10-20 14:25:23 +0900

    finalised config.nix on alien

--- systems/alien/configuration.nix
+++ systems/alien/configuration.nix
@@ -20,7 +20,7 @@
     ../common/optional/desktop/gnome.nix
     ../common/optional/desktop/printers.nix
     ../common/optional/desktop/autologin.nix
-    
+    ./mount-home.nix    
     # Import your generated (nixos-generate-config) hardware configuration
     ./hardware-configuration.nix
   ];
