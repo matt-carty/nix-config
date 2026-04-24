37d22f90ccc14e28e3029806b4fdb46651aec6ec
Matt Cartwright <matt@cartycodes.com>
2026-04-24 12:00:22 +0900

    added medina key to unlock raz

--- systems/razorback/unlock-luks.nix
+++ systems/razorback/unlock-luks.nix
@@ -1,17 +1,19 @@
-{ ...}: {
-
-boot.kernelParams = [ "ip=dhcp" ];
-boot.initrd = {
-  availableKernelModules = [ "r8169" ];
-  network = {
-    enable = true;
-    ssh = {
+{...}: {
+  boot.kernelParams = ["ip=dhcp"];
+  boot.initrd = {
+    availableKernelModules = ["r8169"];
+    network = {
       enable = true;
-      port = 22;
-      authorizedKeys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAxvKmVDcszbYT7/5PfXk/VFJaI59Gzl/mNpg2jxgkOl matt@ipad" ];
-      hostKeys = [ "/etc/secrets/initrd/ssh_host_rsa_key" ];
-      shell = "/bin/cryptsetup-askpass";
+      ssh = {
+        enable = true;
+        port = 22;
+        authorizedKeys = [
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAxvKmVDcszbYT7/5PfXk/VFJaI59Gzl/mNpg2jxgkOl matt@ipad"
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ1mNtM0aqZqCRFBjkpdROq1EQh+Ulr068vuK2hPjzel matt@medina"
+        ];
+        hostKeys = ["/etc/secrets/initrd/ssh_host_rsa_key"];
+        shell = "/bin/cryptsetup-askpass";
+      };
     };
   };
-};
 }
