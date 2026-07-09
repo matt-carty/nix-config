ad1754f37cc69ffc8ea24fe74e5cb5b52b8c6674
Matt Cartwright <matt@cartycodes.com>
2026-07-09 10:55:29 +0900

    claude fix for lockup

--- home/matt/medina.nix
+++ home/matt/medina.nix
@@ -1,4 +1,4 @@
-{...}: {
+{lib, ...}: {
   imports = [
     ./common/global/default.nix
     ./common/features/editing.nix
@@ -12,6 +12,19 @@
   programs.kitty = {
     settings = {
       font_size = 11;
+    };
+  };
+
+  # Quadro K2200 + nvidia 580 + Wayland: DPMS wake and suspend/resume are
+  # unreliable. Keep the display always on and stop the power button from
+  # triggering a broken suspend cycle.
+  dconf.settings = {
+    "org/gnome/desktop/session" = {
+      idle-delay = lib.hm.gvariant.mkUint32 0;
+    };
+    "org/gnome/settings-daemon/plugins/power" = {
+      power-button-action = "nothing";
+      sleep-inactive-ac-type = "nothing";
     };
   };
 }
